3ab0c850c1961a6fff78815e823b584e2b4acf7a
Assanbay Gauhar <asanbayg@gmail.com>
2023-04-23 18:23:03 +0600

    Add filter by name and sort by age

--- lib/screens/patients_screen.dart
+++ lib/screens/patients_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: no_leading_underscores_for_local_identifiers
+
 import 'dart:convert';
 import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
@@ -34,6 +36,83 @@ class _PatientsScreenState extends State<PatientsScreen> {
     });
   }
 
+  void _showMatchingPatients(List<Patient> filteredOrSortedPatients) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return Dialog(
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              width: MediaQuery.of(context).size.width * 0.4,
+              height: MediaQuery.of(context).size.height * 0.2,
+              child: SizedBox(
+                height: MediaQuery.of(context).size.height * 0.1,
+                child: ListView.builder(
+                  itemCount: filteredOrSortedPatients.length,
+                  itemBuilder: (context, index) {
+                    final patient = filteredOrSortedPatients[index];
+                    return ListTile(
+                      title: Text(
+                          '${patient.name} ${patient.surname} - ${patient.age} years'),
+                    );
+                  },
+                ),
+              ),
+            ),
+          );
+        });
+  }
+
+  //sort patients by their age
+  void _sortByAge() {
+    final sortedPatients = List<Patient>.from(patients)
+      ..sort((a, b) => a.age.compareTo(b.age));
+    _showMatchingPatients(sortedPatients);
+  }
+
+  //filter patients by their name
+  void _filterPatients() {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          final TextEditingController _filterController =
+              TextEditingController();
+          return Dialog(
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              width: MediaQuery.of(context).size.width * 0.4,
+              height: MediaQuery.of(context).size.height * 0.2,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  const Text(
+                    'FILTER BY NAME',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                  TextField(
+                    controller: _filterController,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      final filteredPatients = patients
+                          .where((p) => p.name
+                              .toLowerCase()
+                              .contains(_filterController.text.toLowerCase()))
+                          .toList();
+                      _showMatchingPatients(filteredPatients);
+                    },
+                    child: const Text('FILTER'),
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,6 +125,18 @@ class _PatientsScreenState extends State<PatientsScreen> {
           ),
         ),
         centerTitle: true,
+        actions: [
+          IconButton(
+            //filter patients by their name
+            icon: const Icon(Icons.filter_alt),
+            onPressed: _filterPatients,
+          ),
+          IconButton(
+            //sort patients by their age
+            icon: const Icon(Icons.sort),
+            onPressed: _sortByAge,
+          ),
+        ],
       ),
       body: Padding(
         padding: EdgeInsets.symmetric(
@@ -64,8 +155,11 @@ class _PatientsScreenState extends State<PatientsScreen> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Navigator.of(context).push(MaterialPageRoute(
-              builder: (context) => const CreatePatientScreen()));
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => const CreatePatientScreen(),
+            ),
+          );
         },
         child: const Icon(Icons.add),
       ),
